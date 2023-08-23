c311d5dae877351293cc68ae04e82c303663b395
TheOne-Byte <bryanchai18@gmail.com>
2023-08-24 01:46:02 +0700

    Tinggal Order Button

--- routes/web.php
+++ routes/web.php
@@ -60,5 +60,9 @@ Route::get('/addtocart/{user:username}', [OrderController::class,'index'])->midd
 
 Route::resource('/addtocart', OrderController::class)->middleware('auth');
 Route::get('/cart/{user:username}', [OrderController::class,'GetCartByUserId'])->middleware('auth');
-// Route::delete('/cart/deleteItem/{id}', 'OrderController@deleteItem')->name('cart.deleteItem');
+Route::put('/cart/{id}', [OrderController::class, 'update']);
+Route::get('/orderpage/{selectedItems}', 'OrderController@showOrderPage')->name('order.page');
+
+Route::post('/place-order', 'OrderController@placeOrder')->name('place.order');
+
 
