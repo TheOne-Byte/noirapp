815afde03a3ac8c6c39f1d24032a17451075f00f
TheOne-Byte <bryanchai18@gmail.com>
2023-11-04 20:35:48 +0700

    untuk updatediplsyeditem

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\EditDisplayed;
+use App\Models\permission;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,4 +29,70 @@ class UserController extends Controller
 
         return response()->json(['success' => true]);
     }
+
+
+    public function showUpdateForm()
+    {
+        $active = 'editdisplayeditem';
+        return view('updatesingleuser', compact('active')); // Pass the $active variable to the view
+    }
+
+
+
+public function updateSingleUser(Request $request)
+{
+    // Validate the request data
+    $request->validate([
+        'bio' => 'required|string|max:255', // Add more validation rules as needed
+        'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // Max size 2MB for images
+        'video' => 'nullable|mimes:mp4,mov,avi|max:20480', // Max size 20MB for videos
+    ]);
+
+    // Create a new update request
+    $updateRequest = new EditDisplayed();
+    $updateRequest->user_id = auth()->user()->id; // Assuming the user is authenticated
+    $updateRequest->bio = $request->input('bio');
+    $updateRequest->save();
+
+    if ($request->hasFile('image')) {
+        // Store the uploaded image in the storage folder
+        $imagePath = $request->file('image')->store('profile_images', 'public');
+
+        // Save the image path to the update request record
+        $updateRequest->image_path = $imagePath;
+        $updateRequest->save();
+    }
+
+    // Handle video upload if provided
+    if ($request->hasFile('video')) {
+        // Store the uploaded video in the storage folder
+        $videoPath = $request->file('video')->store('profile_videos', 'public');
+
+        // Save the video path to the update request record
+        $updateRequest->video_path = $videoPath;
+        $updateRequest->save();
+    }
+
+
+    // Redirect the user back with a success message or show a confirmation message
+    return redirect()->route('profile.update')->with('success', 'Update request submitted successfully. Waiting for admin approval.');
+}
+
+public function showRequestDetails($requestId)
+{
+    $roleRequest = permission::find($requestId);
+    $updateRequest = EditDisplayed::find($requestId);
+
+    if ($roleRequest && $roleRequest->statcode === 'APV') {
+        // Role request is approved, display image and video
+        return view('request_details', ['image' => $roleRequest->image_path, 'video' => $roleRequest->video_path]);
+    } elseif ($updateRequest && $updateRequest->statcode === 'APV') {
+        // Update request is approved, display updated data
+        return view('request_details', ['bio' => $updateRequest->bio, 'image' => $updateRequest->image_path, 'video' => $updateRequest->video_path]);
+    } else {
+        // Handle other cases or show an error message
+        return view('error_page');
+    }
+}
+
 }
---
+++ resources/views/updatesingleuser.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts/main')
+
+@section('container')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title">Update Profile</h2>
+
+                        <form action="/updatesingleuser" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+
+                            <div class="mb-3 form-group">
+                                <label for="bio" class="form-label">Bio:</label>
+                                <textarea class="form-control" name="bio"></textarea>
+                            </div>
+
+                            <div class="mb-3 form-group">
+                                <label for="image" class="form-label">Skill Dsiplay Image:</label>
+                                <input type="file" class="form-control" name="image">
+                            </div>
+
+                            <div class="mb-3 form-group">
+                                <label for="video" class="form-label"> Skill Display Video:</label>
+                                <input type="file" class="form-control" name="video">
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
